5fcca59544016f7db463008356c83792aa91d96a
gokul <gokulbalaji12345@gmail.com>
2023-08-11 14:20:57 +0530

    new print

---
+++ src/testng_Frameworkk/Class1.java
@@ -0,0 +1,65 @@
+package testng_Frameworkk;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	
+	
+	
+ @Test
+ public void user() {
+	System.out.println("user");
+
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+                    // for take report in test NG
+
+// test output folder - index.html
+// after program run then go to - test output folder - click index.html only not open  - rightclick - open with - web browser
